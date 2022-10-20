0cb2ef47ae0608ad392b1ee06c126259a96151ac
Tony Amos <68511451+agile-anthony@users.noreply.github.com>
2022-10-20 18:23:30 -0400

    URL Reference

--- public/js/njcdd-search.js
+++ public/js/njcdd-search.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
           contentType: "application/json",
           dataType: "json",
           type: "POST",
-          url: "http://account.njcdd.org:3200/api/v1/contact/search"
+          url: "/api/v1/contact/search"
         }
       },
       pageSize: 25,
