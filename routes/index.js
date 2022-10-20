62df7681f43b7be184e28e141d3cafc20ed3844d
Tony Amos <68511451+agile-anthony@users.noreply.github.com>
2022-10-20 16:30:36 -0400

    Expand variables

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,56 @@
 const router = require('express').Router(),
   ldapController = require('../controller/ldap'),
-  mongodbController = require('../controller/mongodb');
+  mongodbController = require('../controller/mongodb'),
+  product = require('../package'),
+  pageVariables = {
+    product: product.name,
+    version: product.version,
+    description: product.description,
+    isManager: false,
+    isOps: false,
+    displayName: null,
+    message: null,
+    content: {}
+  };
 
-/* GET home page. */
+function setVariables({account = null, message = null, content = {}}) {
+  let result = Object.assign({}, pageVariables);
+
+  if(account) {
+    result.isOps = account.isOps;
+    result.isManager = account.isManager;
+    result.displayName = account.displayName;
+  }
+
+  if(message) {
+    result.message = message;
+  }
+
+  if(content) {
+    result.content = content;
+  }
+
+  return result;
+}
+
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', setVariables({}));
 });
 
-router.get('/home', isAuthenticated, function(req, res) {
-  const account = req.session.account;
-  const promise = mongodbController.contactSummary();
-  promise
-    .then((summary) => {
-      res.render('home', {
-        isManager: account.isManager,
-        isOps: account.isOps,
-        displayName: account.displayName,
-        summary: summary
-      });
-    })
-    .catch((error) => {
-      res.render('index', {message: error});
-    });
+router.get('/forgot', function(req, res) {
+  if(req.session.account) {
+    res.redirect('/home');
+  } else {
+    res.render('forgot', setVariables({}));
+  }
 });
 
-router.post('/home', async function(req, res) {
-  const promise = ldapController.authenticateAccount(req.body.email, req.body.pwd);
-  promise
-    .then((account) => {
-      req.session.account = account;
-      res.redirect('/home');
-    })
-    .catch((error) => {
-      res.render('index', {message: error});
-    });
+router.get('/register', function(req, res) {
+  if(req.session.account) {
+    res.redirect('/home');
+  } else {
+    res.render('register', setVariables({}));
+  }
 });
 
 router.get('/logout', function(req, res) {
@@ -43,47 +60,46 @@ router.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-router.get('/forgot', function(req, res) {
-  if(req.session.account) {
-    res.redirect('/home');
-  } else {
-    res.render('forgot');
-  }
-});
-
-router.get('/register', function(req, res) {
-  if(req.session.account) {
-    res.redirect('/home');
-  } else {
-    res.render('register');
-  }
-});
-
-router.get('/admin', isManager, function(req, res) {
+router.get('/home', isAuthenticated, function(req, res) {
   const account = req.session.account;
-  res.render('admin', {
-    isManager: account.isManager,
-    isOps: account.isOps,
-    displayName: account.displayName
-  });
+  const promise = mongodbController.contactSummary();
+
+  console.log(account);
+  promise
+    .then((summary) => {
+      res.render('home', setVariables({account: account, content: summary}));
+    })
+    .catch((error) => {
+      res.render('index', setVariables({message: error}));
+    });
+});
+
+router.post('/home', async function(req, res) {
+  const promise = ldapController.authenticateAccount(req.body.email, req.body.pwd);
+
+  promise
+    .then(account => {
+      req.session.account = account;
+      res.redirect('/home');
+    })
+    .catch((error) => {
+      res.render('index', setVariables({message: error}));
+    });
 });
 
 router.get('/search', isOps, function(req, res) {
   const account = req.session.account;
-  res.render('search', {
-    isManager: account.isManager,
-    isOps: account.isOps,
-    displayName: account.displayName
-  });
+  res.render('search', setVariables({account: account}));
 });
 
 router.get('/profile', isAuthenticated, function(req, res) {
   const account = req.session.account;
-  res.render('profile', {
-    isManager: account.isManager,
-    isOps: account.isOps,
-    displayName: account.displayName
-  });
+  res.render('profile', setVariables({account: account}));
+});
+
+router.get('/admin', isManager, function(req, res) {
+  const account = req.session.account;
+  res.render('admin', setVariables({account: account}));
 });
 
 function isAuthenticated(req, res, next) {
