4e321124e4ff322fc200f58a0142246e5b6086fa
Tony Amos <68511451+agile-anthony@users.noreply.github.com>
2022-11-08 18:40:27 -0500

    Move API call to backend

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals = {
     message: err.message,
-    error: req.app.get('env') === 'development' ? err : {}
+    error: req.app.get('env') !== 'production' ? err : {}
   };
 
   // render the error page
