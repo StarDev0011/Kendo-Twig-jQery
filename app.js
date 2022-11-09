c4849530260b54191644716059d8a027f9416372
Tony Amos <68511451+agile-anthony@users.noreply.github.com>
2022-11-08 23:19:36 -0500

    Add logger

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const logger = require('morgan');
 const session = require('express-session');
 const redisController = require('./controller/redis');
 let RedisStore = require("connect-redis")(session);
@@ -25,7 +24,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
